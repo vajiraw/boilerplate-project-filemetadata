3ec756be1636227d05834d6629022a5deb628ac7
Vajira <vajirawke@gmail.com>
2022-10-14 12:06:45 +0530

    initial logic implement for file meta data

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+
+const multer  = require('multer');
+const { type } = require('express/lib/response');
+const upload = multer({ dest: 'uploads/' })
+
 var app = express();
 
 app.use(cors());
@@ -11,8 +16,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
+  //console.log(req);
+  const upfile = req.file
+  if(upfile===undefined){
+    return res.json({'Error':'No $ile selected for uploaded'})
+  }
+  res.json({'name':upfile.originalname,'type': upfile.mimetype,'size':upfile.size} )
 
-
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
